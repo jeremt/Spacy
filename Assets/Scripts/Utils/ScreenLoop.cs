302e7ab0be1be39e4a144518a9048ba640dbdaab
Jeremie Taboada <taboada.jeremie@gmail.com>
2016-01-06 12:36:38 +0100

    Added Screen loop.

---
+++ Assets/Scripts/Utils/ScreenLoop.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenLoop : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        float xDelta = transform.position.x;
+        float yDelta = transform.position.y;
+        float aspectRatio = (float)Screen.width / (float)Screen.height;
+        if (transform.position.x < -1f * aspectRatio) {
+            xDelta = 1f * aspectRatio;
+        } else if (transform.position.x > 1f * aspectRatio) {
+            xDelta = -1f * aspectRatio;
+        }
+        if (transform.position.y < -1f) {
+            yDelta = 1f;
+        } else if (transform.position.y > 1f) {
+            yDelta = -1f;
+        }
+        transform.position = new Vector3(xDelta, yDelta, 0);
+	}
+}
