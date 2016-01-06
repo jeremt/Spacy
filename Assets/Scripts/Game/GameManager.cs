8398f9b3647f82a009be1a5b10b201fb040f7b7f
Jeremie Taboada <taboada.jeremie@gmail.com>
2016-01-06 20:14:29 +0100

    Finish player select and add GameManager.

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerData {
+    public int InputIndex;
+    public Color SkinColor;
+
+    public PlayerData(int inputIndex, Color skinColor) {
+        InputIndex = inputIndex;
+        SkinColor = skinColor;
+    }
+
+}
+
+public class GameManager : Singleton<GameManager> {
+
+    private PlayerData[] _players = new PlayerData[5];
+
+    protected GameManager() {}
+
+    public void SetPlayer(int index, PlayerData player) {
+        _players[index] = player;
+    }
+
+    public void RemovePlayer(int index) {
+        _players[index] = null;
+    }
+
+    public PlayerData GetPlayer(int index) {
+        return _players[index];
+    }
+
+    public int GetNextPlayerIndex() {
+        for (int i = 0; i < 5; ++i) {
+            if (_players[i] == null) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public bool InputAvailable(int inputIndex) {
+        for (int i = 0; i < 5; ++i) {
+            if (_players[i] != null && _players[i].InputIndex == inputIndex) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
--- Assets/Scripts/Menu/PlayerSelector.cs
+++ Assets/Scripts/Menu/PlayerSelector.cs
@@ -5,36 +5,53 @@ using System.Collections;
 public class PlayerSelector : MonoBehaviour {
 
     public int Index = 0;
-        
+    private Image _playerImage;
+    public Text Text;
+    public Image DeviceImage;
 
-    [HideInInspector] public int InputIndex = -1;
+    public Color[] _colors = new Color[] {
+        new Color(1, 0, 0, 1),
+        new Color(0, 1, 0, 1),
+        new Color(0, 0, 1, 1)
+    };
 
-	// Use this for initialization
-	void Start() {
-	
-	}
-	
+    public Sprite[] DevicesSprite;
+    	
+    void Awake() {
+        _playerImage = transform.Find("PlayerImage").GetComponent<Image>();
+    }
+
 	// Update is called once per frame
 	void Update() {
-        if (InputIndex != -1) {
-            if (Input.GetAxis(InputIndex + "_Shoot")) { // Cancel
-                _deselectInput();
+        PlayerData player = GameManager.Instance.GetPlayer(Index);
+        if (player != null) {
+            if (Input.GetButtonUp(player.InputIndex + "_Shoot")) { // Cancel
+                _deselectPlayer();
             }
         } else {
-            for (int inputIndex = 0; inputIndex < 5; ++inputIndex) {
-                if (Input.GetAxis(inputIndex + "_Jump")) { // Submit
-                    _selectInput(inputIndex);
+            // Si tout les players avant moi sont pris, je selecte
+            if (GameManager.Instance.GetNextPlayerIndex() == Index) {
+                for (int inputIndex = 0; inputIndex < 2; ++inputIndex) {
+                    if (Input.GetButtonUp(inputIndex + "_Jump") && GameManager.Instance.InputAvailable(inputIndex)) { // Submit
+                        _selectPlayer(inputIndex);
+                    }
                 }
             }
         }
 	}
 
-    private void _selectInput(int index) {
-        Debug.Log("Select Input " + index);
+    private void _selectPlayer(int inputIndex) {
+        GameManager.Instance.SetPlayer(Index, new PlayerData(inputIndex, _colors[0]));
+        _playerImage.color = GameManager.Instance.GetPlayer(Index).SkinColor;
+        Text.color = GameManager.Instance.GetPlayer(Index).SkinColor;
+        DeviceImage.overrideSprite = DevicesSprite[inputIndex];
     }
 
-    private void _deselectInput() {
-        Debug.Log("Deselect input " + InputIndex);
+    private void _deselectPlayer() {
+        _playerImage.color = new Color(1f, 1f, 1f, 125f/255f);
+        Text.color = new Color(1f, 1f, 1f, 125f/255f);
+        DeviceImage.overrideSprite = DevicesSprite[5];
+        GameManager.Instance.RemovePlayer(Index);
     }
 
 }
