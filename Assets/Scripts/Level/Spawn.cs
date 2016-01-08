89e999d2bd38156c0281c645f2e2c49f5f902f79
Jeremie Taboada <taboada.jeremie@gmail.com>
2016-01-08 01:06:34 +0100

    Handle player spawn.

---
+++ Assets/Scripts/Level/Spawn.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+
+    void OnDrawGizmos() {
+//        Gizmos.color = ;
+        Gizmos.DrawSphere(transform.position, 0.1f);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerSpawner : MonoBehaviour {
+
+    public Transform[] Spawns;
+    public GameObject Player;
+
+	void Start() {
+        var spawns = new List<Transform>();
+        spawns.AddRange(Spawns);
+        for (int playerIndex = 0; playerIndex < GameManager.Instance.GetNextPlayerIndex(); ++playerIndex) {
+            Transform spawnTransform = spawns[Random.Range(0, spawns.Count)];
+            spawns.Remove(spawnTransform);
+            var player = Instantiate(Player, spawnTransform.position, Quaternion.Euler(new Vector3(0,0,0))) as GameObject;
+            player.GetComponent<SpriteRenderer>().color = GameManager.Instance.GetPlayer(playerIndex).SkinColor;
+            var inputIndex = GameManager.Instance.GetPlayer(playerIndex).InputIndex;
+            player.GetComponent<PlayerGun>().InputIndex = inputIndex;
+            player.GetComponent<Player>().InputIndex = inputIndex;
+            player.GetComponent<Player>().Index = playerIndex;
+        }
+	}
+
+}
