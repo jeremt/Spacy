d6118567a4badaba6b9eeb9031f26b32c38f14e6
Jeremie Taboada <taboada.jeremie@gmail.com>
2016-01-08 13:11:45 +0100

    Added very basic scene to debug animations.

---
+++ Assets/Scripts/Player/DebugPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugPlayer : MonoBehaviour {
+
+    public Player Player;
+
+    // Adds one default player (when the level is not created from the menu)
+	void Start () {
+        GameManager.Instance.SetPlayer(0, 0);
+        Player.GetComponent<SpriteRenderer>().color = GameManager.Instance.GetPlayer(0).SkinColor;
+        Player.GetComponent<PlayerGun>().InputIndex = 0;
+        Player.GetComponent<Player>().InputIndex = 0;
+        Player.GetComponent<Player>().Index = 0;
+	}
+
+}
